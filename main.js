c8d7d24bda97d52185170756f8282d55d1009631
altjian <hello@altjian.com>
2019-03-29 11:37:35 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+let win
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 960,
+        height: 600,
+        minWidth: 830,
+        minHeight: 500,
+        backgroundColor: '#f8f8f8'
+    })
+
+    win.loadURL(`file://${__dirname}/main.html`)
+}
+
+app.on('ready', createWindow)
